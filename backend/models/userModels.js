a69a1445b9b0e5b97e35524848edfbbea4353059
Neha Kumari Nandini <nehakn@gmail.com>
2025-12-28 23:47:36 +0530

    Rewrite server.js with proper CORS and Authorization support

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,11 +1,13 @@
- import mongoose from "mongoose";
+import mongoose from "mongoose";
 
- const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-    cartData:{type:Object,default:{}}
- },{minimize:false})
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  cartData: { type: Object, default: {} },
+});
 
-const orderModel = mongoose.models.order || mongoose.model("order", orderSchema);
- export default userModel;
+const userModel =
+  mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;

7d7765183001173eb5233371fa4aeaeac7878f00
Neha Kumari Nandini <nehakn@gmail.com>
2025-12-29 15:16:14 +0530

    Your commit message

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
-const url = import.meta.env.VITE_API_URL;
+  const url = import.meta.env.VITE_API_URL;
 
   return (
     <div>
-      <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <div className="app-content">
         <Sidebar />
 
         <Routes>
-          <Route path="/add" element={<Add url = {url}/>} />
-          <Route path="/list" element={<List url = {url} />} />
-          <Route path="/orders" element={<Orders url = {url} />} />
+          
+          <Route path="/" element={<Navigate to="/add" />} />
+
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
 
       </div>

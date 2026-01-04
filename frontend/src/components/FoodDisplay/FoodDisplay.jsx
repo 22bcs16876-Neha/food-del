cfae3d9803a568cd7cd8ca1b850f804e901c4bef
Neha Kumari Nandini <nehakn@gmail.com>
2026-01-04 12:40:02 +0530

    display

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,25 +1,26 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/storeContext'
-import FoodItem from '../FoodItem/FoodItem'
-const FoodDisplay = ({category}) => {
-  
-    const {food_list} = useContext(StoreContext)
-    return (
-    <div className='food-display' id = 'food-display'>
-        <h2>Top dishes near you</h2>   
-        <div className="food-display-list">
-            {food_list.map((item , index)=>{
-                if(category === "All" || category === item.category){
-                                        return<FoodItem key = {index} id = {item._id} name = {item.name}
-                description = {item.description} price={item.price} image={item.image}/>
+import React from "react";
+import "./FoodItem.css";
 
-                }
-            }
-            )}
-            </div>   
+const FoodItem = ({ id, name, description, price, image }) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img
+          src={`${backendUrl}/uploads/${image}`}
+          alt={name}
+          className="food-item-image"
+        />
+      </div>
+
+      <div className="food-item-info">
+        <p className="food-item-name">{name}</p>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">₹{price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodItem;

953281ff301aadf71ebed3c8c585016b7005e14e
Neha Kumari Nandini <nehakn@gmail.com>
2026-01-04 12:34:43 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,11 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
+ res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Content-Type, Authorization, token"
+);
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,POST,PUT,DELETE,OPTIONS"

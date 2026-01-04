f6525866bf14c246f34bda4f6b732429e70aa9ac
Neha Kumari Nandini <nehakn@gmail.com>
2026-01-04 12:45:49 +0530

    Fix image loading by correcting backend static path usage

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,21 +1,24 @@
-import React, { useContext } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../context/storeContext';
+import React, { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/storeContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-
-//   const [itemCount, setItemCount] = useState(0);
-const { cartItems, addToCart, removeFromCart ,url } = useContext(StoreContext);
+  const { cartItems, addToCart, removeFromCart, url } =
+    useContext(StoreContext);
 
   return (
-    <div className='food-item'>
+    <div className="food-item">
       <div className="food-item-img-container">
-        <img className='food-item-image' src={ url+"/images/"+image} alt={name} />
+        <img
+          className="food-item-image"
+          src={`${url}/uploads/${image}`}
+          alt={name}
+        />
 
         {!cartItems[id] ? (
           <img
-            className='add'
+            className="add"
             onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt="add"
@@ -25,11 +28,11 @@ const { cartItems, addToCart, removeFromCart ,url } = useContext(StoreContext);
             <img
               onClick={() => removeFromCart(id)}
               src={assets.remove_icon_red}
-              alt="remove"  />
+              alt="remove"
+            />
             <p>{cartItems[id]}</p>
             <img
-             onClick={() => addToCart(id)}
-
+              onClick={() => addToCart(id)}
               src={assets.add_icon_green}
               alt="add"
             />
@@ -46,7 +49,7 @@ const { cartItems, addToCart, removeFromCart ,url } = useContext(StoreContext);
         <p className="food-item-price">₹{price}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
